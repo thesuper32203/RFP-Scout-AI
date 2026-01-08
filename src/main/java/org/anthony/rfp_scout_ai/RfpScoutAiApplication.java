f68d89e1368ead1000f43a4e25340223df73da8f
Anthony <thesuper32203@gmail.com>
2026-01-07 20:10:31 -0500

    first commit

---
+++ src/main/java/org/anthony/rfp_scout_ai/RfpScoutAiApplication.java
@@ -0,0 +1,13 @@
+package org.anthony.rfp_scout_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfpScoutAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RfpScoutAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anthony/rfp_scout_ai/RfpScoutAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anthony.rfp_scout_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfpScoutAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
